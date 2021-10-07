b65a0e18673915d99a5bcdd7bdd10d24d0c11204
bahdotsh <appu.yess@gmail.com>
2021-10-07 17:10:43 +0530

    .gitignore applied

--- app/actors/ActorCaps.scala
+++ app/actors/ActorCaps.scala
@@ -1,5 +1,26 @@
 package actors
 
-object ActorCaps {
+import akka.actor.{Actor, ActorSystem, Props}
+import controllers.CreatePersonForm
 
+
+object ActorCaps  extends  App {
+  class SimpleActor extends  Actor {
+    def receive: Receive = {
+      case message: String =>
+        println(s"I have recieved a message $message")
+      case SpecialMessage(contents) =>
+        println(s"I have a special message $contents")
+    }
+  }
+
+  val system = ActorSystem("ActorCapsDemo")
+  val actorCapsA = system.actorOf(Props[SimpleActor](), "actorCapsA")
+
+  actorCapsA ! "I kicked it"
+
+  case class SpecialMessage(contents: String)
+  actorCapsA ! SpecialMessage("Special Content")
+
+  actorCapsA ! "allPersons"
 }
--- app/actors/ActorIntro.scala
+++ app/actors/ActorIntro.scala
@@ -1,5 +1,38 @@
 package actors
 
-object ActorIntro {
+import akka.actor.{Actor, ActorSystem, Props}
 
+object ActorsIntro extends App {
+  val actorSystem = ActorSystem("firstActorSystem")
+  println(actorSystem.name)
+
+  class WordCountActor extends Actor {
+    var totalWords = 0
+      def receive : Receive = {
+        case message: String =>
+          println(s"message $message")
+          totalWords += message.split(" ").length
+        case msg => println("UnknownMessage")
+      }
+  }
+
+  val wordCounter = actorSystem.actorOf(Props[WordCountActor](), "wordCounter")
+
+  wordCounter ! "Loving it"
+
+  object Person {
+    def props(name: String) = Props(new Person(name))
+  }
+
+  class Person(name : String) extends Actor {
+    def receive: Receive = {
+      case "hi" => println(s"Name is $name")
+    }
+  }
+
+  val personA = actorSystem.actorOf(Props(new Person("Bob")))
+  val personB = actorSystem.actorOf(Person.props("rob"))
+
+  personA ! "hi"
+  personB ! "hi"
 }
